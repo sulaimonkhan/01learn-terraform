fed5b810d2df719e553d585beb0165847aa251cf
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-09-03 15:57:25 +0530

    demo

---
+++ 02-outputs/outputs.tf
@@ -0,0 +1,12 @@
+output "sample" { 
+  value = "HelloWorld"
+}
+
+## Any string in terraform need to be quoted with Double quotes only.
+# single quotes are not supported
+
+output "sample" {
+  value = 1000    
+}
+
+# numbers and booleans, ( true , false )
