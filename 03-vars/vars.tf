3721e87ffcf778a9091e342554268dd9e2641894
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-09-09 11:22:19 +0530

    demo

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -70,10 +70,10 @@ output "course" {
 }
 
 output "courses" {
-  value = var.courses
+  value = var.courses[2]
 }
 
 output "course_details" {
-  value = var.course_details
+  value = var.course_details["DevOps"]
 }
 
